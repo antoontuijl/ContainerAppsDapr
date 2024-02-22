db13580cb1b38a7bb4ac44af8f5a4c1552bd8861
Antoon Tuijl <antoontuijl@outlook.com>
2024-02-22 06:54:37 +0100

    initial checkin

---
+++ src/TodoApp.Backend/Controllers/TodosController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApp.Backend.Controllers
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Done { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Todo> _todoList = new List<Todo>();
+
+            _todoList.Add(new Todo
+            {
+                Id = 1,
+                Name = "Learn JavaScript",
+                Done = true,
+            });
+            _todoList.Add(new Todo
+            {
+                Id = 2,
+                Name = "Learn React",
+                Done = false
+            });
+
+            return Ok(_todoList);
+        }
+    }
+}
